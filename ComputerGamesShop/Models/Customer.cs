5eb25b8fabb0d7b628371ea73f946dd9c7af6597
Dolev Efrati <defrati01@gmail.com>
2018-12-27 23:28:04 +0200

    add Game, Customer and Publisher classes

---
+++ ComputerGamesShop/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComputerGamesShop.Models
+{
+    public enum Gender
+    {
+        male,
+        female
+    };
+
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName
+        {
+            get
+            {
+                return this.FirstName + " " + this.LastName;
+            }
+        }
+        public DateTime BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PhoneNumber { get; set; }
+        //public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ ComputerGamesShop/Models/Publisher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComputerGamesShop.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public DateTime FoundedDate { get; set; }
+        public Genre Specialty { get; set; }
+        public ICollection<Game> Games { get; set; }
+    }
+}

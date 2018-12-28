cbc9f255038c9a646fc9b43d099db36237af27b3
Dolev Efrati <defrati01@gmail.com>
2018-12-28 09:32:49 +0200

    change the folders in the project

---
+++ ComputerGamesShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ComputerGamesShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
